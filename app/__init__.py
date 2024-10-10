30e05518aef28598f1c93645a60b457bff29c536
1cbyc <isaacnsisong@gmail.com>
2024-10-10 10:41:41 +0100

    building a feedback collection platform to integrate feedback system

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 # from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # using sqlite for simplicity at this point
+app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 # login_manager = LoginManager(app)
 # login_manager.login_view = 'login'  # will redirect to login page once the user is not authenticated
