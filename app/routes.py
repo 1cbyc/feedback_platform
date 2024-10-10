d420638e7e3fce06f40aeb3a7fdd56e8a8794520
1cbyc <isaacnsisong@gmail.com>
2024-10-10 10:15:30 +0100

    building a feedback collection platform to integrate feedback system

--- app/routes.py
+++ app/routes.py
@@ -48,6 +48,13 @@ def login():
     
     return render_template('login.html', form=form)
 
+# for logout route
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
 
 @app.route('/feedback', methods=['GET', 'POST'])
 @login_required
