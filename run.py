1a096f4b2aae5c0ad01e444ea454a473991505a2
1cbyc <isaacnsisong@gmail.com>
2024-10-10 10:21:12 +0100

    building a feedback collection platform to integrate feedback system

--- run.py
+++ run.py
@@ -25,11 +25,7 @@ db = SQLAlchemy(app)
 # will init Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'  # the route name for login
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+login_manager.login_view = 'login'  # The route name for login
 
 if __name__ == '__main__':
     app.run(debug=True)
