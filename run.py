7c50c0d449e024560f3faacf8e7a3910cffa1dfa
1cbyc <isaacnsisong@gmail.com>
2024-10-10 10:21:35 +0100

    building a feedback collection platform to integrate feedback system

--- run.py
+++ run.py
@@ -27,6 +27,10 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'  # The route name for login
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 if __name__ == '__main__':
     app.run(debug=True)
 
