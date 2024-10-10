57307a2690e436212edb0da159b945b4376a675f
1cbyc <isaacnsisong@gmail.com>
2024-10-10 10:42:54 +0100

    building a feedback collection platform to integrate feedback system

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request
-# from flask_login import login_user, current_user, logout_user, login_required, current_user
 from flask_login import login_user, current_user, logout_user, login_required
 from app import app, db
+# from flask_login import login_user, current_user, logout_user, login_required, current_user
 # from models import User
 # from forms import RegistrationForm, LoginForm
 from app.forms import RegistrationForm
