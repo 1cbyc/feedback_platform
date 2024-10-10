bf399de14a9b03b5ec47e96acfcbc9c6ec5e8a8c
1cbyc <isaacnsisong@gmail.com>
2024-10-10 09:29:38 +0100

    building a feedback collection platform to integrate feedback system

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # using sqlite for simplicity
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # using sqlite for simplicity at this point
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'  # Redirect to login page if user is not authenticated
+login_manager.login_view = 'login'  # will redirect to login page once the user is not authenticated
 
 from app import routes  # Import routes at the end to avoid circular imports
